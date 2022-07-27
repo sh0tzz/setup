368e4924425196edeaac34c1a0a504a988eb136f
sh0tzz <lubranier@gmail.com>
2022-07-27 21:01:01 +0200

    .

--- laptop/scripts/08-nitrogen.sh
+++ laptop/scripts/08-nitrogen.sh
@@ -1,6 +1,6 @@
 sudo apt install nitrogen -y
 mkdir ~/.config/nitrogen
-sudo mv ~/setup/dotfiles/bg-saved.cfg ~/.config/nitrogen/bg-saved.cfg
-sudo mv ~/setup/dotfiles/nitrogen.cfg ~/.config/nitrogen/nitrogen.cfg
-ln -s ~/.config/nitrogen/bg-saved.cfg ~/setup/dotfiles/bg-saved.cfg
-ln -s ~/.config/nitrogen/nitrogen.cfg ~/setup/dotfiles/nitrogen.cfg
+sudo mv ~/setup/laptop/dotfiles/bg-saved.cfg ~/.config/nitrogen/bg-saved.cfg
+sudo mv ~/setup/laptop/dotfiles/nitrogen.cfg ~/.config/nitrogen/nitrogen.cfg
+ln -s ~/.config/nitrogen/bg-saved.cfg ~/setup/laptop/dotfiles/bg-saved.cfg
+ln -s ~/.config/nitrogen/nitrogen.cfg ~/setup/laptop/dotfiles/nitrogen.cfg
--- laptop/scripts/11-dev.sh
+++ laptop/scripts/11-dev.sh
@@ -1,3 +1,4 @@
 sudo apt install build-essential -y
 sudo apt install python3-pip -y
 sudo apt install "libsdl*" -y
+sudo apt install cmake -y
